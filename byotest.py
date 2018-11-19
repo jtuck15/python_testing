2dca96cc05bd2ddad63fc4596728533301fb85d0
Jimmy Tuck <jim.tuck15@gmail.com>
2018-11-19 22:20:04 +0000

    added my own framework tests

--- byotest.py
+++ byotest.py
@@ -1,7 +1,24 @@
-def number_of_evens(numbers):
-    return 0
-    
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
     
-test_are_equal(number_of_evens([1, 2, 3, 4, 5]), 2)
+def test_not_equal(a, b):
+    assert a != b, "Did not expect {0} but got {1}".format(a, b)
+    
+def test_is_in(collection, item):
+    assert item in collection, "{0} does not contain {1}".format(collection, item)
+    
+def test_not_in(collection, item):
+    assert item not in collection, "{0} should not contain the number {1}".format(collection, item)
+    
+def test_between(lower, actual, upper):
+    assert lower <= actual <= upper, "{0} is not between {1} and {2}".format(lower, actual, upper)
+    
+test_not_equal(4, 3)
+
+test_is_in([1, 2, 7, 9], 7)
+
+test_not_in([4, 3], 1)
+
+test_between(10, 15, 20)
+
+print("All tests passed!")
